a44dd746c1e7d2481667c41fb9b6bb170a91e33a
Victor Perez <zunaty@gmail.com>
2021-12-05 13:29:49 -0700

    Updating employee class

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace CatWorx.BadgeMaker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // This is our employee-getting code now
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+
+        static List<string> GetEmployees()
+        {
             List<string> employees = new List<string>();
-
-            while(true)
+            while (true)
             {
-                Console.WriteLine("Please enter a name: ");
+                Console.WriteLine("Please enter a first name: (leave empty to exit): ");
                 string input = Console.ReadLine();
-
                 if (input == "")
                 {
                     break;
                 }
 
-                employees.Add(input);
+                Employee currentEmployee = new Employee(input, "Smith");
+                employees.Add(currentEmployee.GetName());
             }
 
-            for (int i = 0; i < employees.Count; i++)
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i =0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
             }
