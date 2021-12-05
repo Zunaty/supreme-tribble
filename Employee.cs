81e4592f3cb3e3a0686cffa4ebf8e1513ca5a03a
Victor Perez <zunaty@gmail.com>
2021-12-05 13:55:53 -0700

    Gathering and displaying employee data

--- Employee.cs
+++ Employee.cs
@@ -2,17 +2,27 @@ namespace CatWorx.BadgeMaker
 {
     class Employee
     {
-        public string FirstName;
-        public string LastName;
-        public int ID;
-        public string PhotoUrl;
-        public Employee(string firstName, string lastName) {
+        private string FirstName;
+        private string LastName;
+        private int ID;
+        private string PhotoUrl;
+        public Employee(string firstName, string lastName, int id, string photoUrl) {
             FirstName = firstName;
             LastName = lastName;
+            ID = id;
+            PhotoUrl = photoUrl;
         }
 
         public string GetName() {
             return FirstName + " " + LastName;
+        }
+
+        public int GetId() {
+            return ID;
+        }
+
+        public string GetPhotoUrl() {
+            return PhotoUrl;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,34 +8,44 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         {
             // This is our employee-getting code now
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
 
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
                 Console.WriteLine("Please enter a first name: (leave empty to exit): ");
-                string input = Console.ReadLine();
-                if (input == "")
+                string firstName = Console.ReadLine();
+                if (firstName == "")
                 {
                     break;
                 }
 
-                Employee currentEmployee = new Employee(input, "Smith");
-                employees.Add(currentEmployee.GetName());
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Enter photo url: ");
+                string photoUrl = Console.ReadLine();
+
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
             }
 
             return employees;
         }
 
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i =0; i < employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
     }
